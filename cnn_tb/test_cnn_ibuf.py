662112ac6e302508eef625a2ba31b930e71a950a
Ludi-1 <liou.hu@protonmail.com>
2023-07-10 23:57:09 +0200

    cnn ibuf

--- cnn_tb/test_cnn_ibuf.py
+++ cnn_tb/test_cnn_ibuf.py
@@ -6,5 +6,6 @@ from cocotb.types import LogicArray
 @cocotb.test()
 async def cnn_ibuf_test_1(dut):
     cocotb.start_soon(Clock(dut.i_clk, 1, units='ns').start())
-    dut.i_data <= "111111110000000010101010"
+    dut.i_data.value = LogicArray("11110010")
+    dut.i_write_enable.value = 1
     await Timer(10**4, units="ns")  # wait a bit
