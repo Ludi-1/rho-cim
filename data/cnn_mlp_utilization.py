8742dfbb38e807723719f1588812cbbb5209395f
M.Z. (Mahdi) Zahedi <mzahedi@ce-cuda01.ewi.tudelft.nl>
2023-10-27 09:43:50 +0200

    making the utilization figures more readable

--- data/cnn_mlp_utilization.py
+++ data/cnn_mlp_utilization.py
@@ -13,38 +13,42 @@ regs = [150, 210, 244, 2022, 4264]
 mux7 = [211, 436, 693, 129, 520]
 mux8 = [98, 3, 301, 49, 189]
 
-plt.bar(categories, luts)
-plt.xlabel('Neural network')
-plt.ylabel('LUTs')
+plt.bar(categories, luts, width=0.6, color="royalblue")
+plt.xlabel('Neural network', fontsize=12)
+plt.ylabel('LUTs', fontsize=14)
 plt.title('LUT utilization for different neural networks')
-
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=14)
 plt.savefig("plots/cnn_mlp_utilization_luts.svg", format="svg")
 # Display the plot
 plt.close()
 
-plt.bar(categories, regs)
-plt.xlabel('Neural network')
-plt.ylabel('Registers')
+plt.bar(categories, regs, width=0.6, color="darkred")
+plt.xlabel('Neural network', fontsize=12)
+plt.ylabel('Registers', fontsize=14)
 plt.title('Register utilization for different neural networks')
-
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=14)
 plt.savefig("plots/cnn_mlp_utilization_regs.svg", format="svg")
 # Display the plot
 plt.close()
 
-plt.bar(categories, mux7)
-plt.xlabel('Neural network')
-plt.ylabel('F7 Muxes')
+plt.bar(categories, mux7, width=0.6, color="teal")
+plt.xlabel('Neural network', fontsize=12)
+plt.ylabel('F7 Muxes', fontsize=14)
 plt.title('F7 Mux utilization for different neural networks')
-
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=14)
 plt.savefig("plots/cnn_mlp_utilization_mux7.svg", format="svg")
 # Display the plot
 plt.close()
 
-plt.bar(categories, mux8)
-plt.xlabel('Neural network')
-plt.ylabel('F8 Muxes')
+plt.bar(categories, mux8, width=0.6, color="skyblue")
+plt.xlabel('Neural network', fontsize=12)
+plt.ylabel('F8 Muxes', fontsize=14)
 plt.title('F8 Mux utilization for different neural networks')
-
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=14)
 plt.savefig("plots/cnn_mlp_utilization_mux8.svg", format="svg")
 # Display the plot
 plt.close()
