1d7a91ff5d0f3db7e8c044e00984f499843f0d78
Ludi-1 <liou.hu@protonmail.com>
2024-03-14 23:36:04 +0100

    perf-sim: Analysis done

--- perf-sim/analysis.py
+++ perf-sim/analysis.py
@@ -1,20 +1,54 @@
 import csv
 from math import ceil, log2
 
-def analysis(conf_name, param_dict, technology, datatype_size, crossbar_size, conv_dt):
-    f = open(f"./analysis/{conf_name}.txt", "w")
-    fieldnames = ['Layer', 'xbar', "s&a", "func"]
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
+def analysis(conf_name, param_dict, datatype_size, crossbar_size, conv_dt):
+    f = open(f"./analysis/{conf_name}.csv", "w")
+    fieldnames = ['Layer', 'i_xbar', 'o_xbar', "i_s&a", "o_s&a", "i_func", "o_func"]
+    writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';')
     writer.writeheader()
     for layer in param_dict["layer_list"]:
         match layer[0]:
             case "conv":
-                writer.writerow({'Layer': layer[0], 'I': 10})
+                # inputs_pixels = layer[1]**2
+                output_pixels = ((layer[1] - layer[2])/layer[5] + 1)**2
+                input_channels = layer[3]
+                output_channels = layer[5]
+                kernel_size = layer[2]
+                n_v = ceil(
+                    kernel_size**2 * input_channels / crossbar_size
+                )
+                n_h = ceil(
+                    output_channels
+                    * datatype_size
+                    / crossbar_size
+                )
+                i_xbar = output_pixels * kernel_size**2 * datatype_size * (n_h * input_channels + output_channels)
+                i_sna = output_pixels * n_v * datatype_size**2 * (conv_dt + log2(crossbar_size)) / conv_dt
+                i_func = output_pixels * (datatype_size + log2(crossbar_size)) * n_v
+                writer.writerow({
+                    'Layer': f"{layer[0]} {kernel_size}x{kernel_size}",
+                    'i_xbar': i_xbar,
+                    'o_xbar': i_sna,
+                    'i_s&a': i_sna,
+                    'o_s&a': i_func,
+                    'i_func': i_func,
+                    'o_func': output_pixels * output_channels * datatype_size
+                })
             case "pool":
-                writer.writerow({'Layer': layer[0], 'xbar': 0, 's&a': 0, 'func': 1})
+                inputs = layer[1]**2 * layer[2]
+                outputs = ((layer[1] - layer[2])/layer[5] + 1)**2 * layer[2]
+                writer.writerow({
+                    'Layer': f"{layer[0]} {layer[2]}x{layer[2]}",
+                    'i_xbar': 0,
+                    'o_xbar': 0,
+                    'i_s&a': 0,
+                    'o_s&a': 0,
+                    'i_func': inputs * datatype_size,
+                    'o_func': outputs * datatype_size 
+                })
             case "fc":
                 inputs = layer[1] * layer[3] # input neurons * input channels
-                outputs = layer[3]
+                outputs = layer[2]
                 n_v = ceil(
                     inputs / crossbar_size
                 )
@@ -25,7 +59,15 @@ def analysis(conf_name, param_dict, technology, datatype_size, crossbar_size, co
                 )
                 i_xbar = datatype_size * inputs * (n_h + outputs)
                 i_sna = outputs * n_v * datatype_size**2 * (conv_dt + log2(crossbar_size)) / conv_dt
-                i_func = outputs * (datatype_size + log2(crossbar_size) + ceil(log2(n_v)))
-                writer.writerow({'Layer': layer[0], 'xbar': i_xbar, 's&a': i_sna, 'func': i_func})
+                i_func = outputs * (datatype_size + log2(crossbar_size)) * n_v
+                writer.writerow({
+                    'Layer': f"{layer[0]} ({outputs})",
+                    'i_xbar': i_xbar,
+                    'o_xbar': i_sna,
+                    'i_s&a': i_sna,
+                    'o_s&a': i_func,
+                    'i_func': i_func,
+                    'o_func': outputs * datatype_size
+                })
             case _:
                 raise ValueError(f"Bad layer type {layer[0]}")
--- perf-sim/main.py
+++ perf-sim/main.py
@@ -42,11 +42,11 @@ def main():
 
     for param_dict_tuple in param_dicts:
         param_dict = param_dict_tuple[1]
-        for technology in technology_list:
-            for datatype_size in datatype_size_list:
-                for crossbar_size in crossbar_size_list:
-                    for conv_dt in [2, 4, 8]:
-                        conf_name = f"{technology}_{param_dict_tuple[0]}_d{datatype_size}_c{crossbar_size}_a{conv_dt}"
-                        analysis(conf_name, param_dict, technology, datatype_size, crossbar_size, sparsity)
+        for datatype_size in datatype_size_list:
+            for crossbar_size in crossbar_size_list:
+                for conv_dt in [2, 4, 8]:
+                    conf_name = f"{param_dict_tuple[0]}_d{datatype_size}_c{crossbar_size}_a{conv_dt}"
+                    analysis(conf_name, param_dict, datatype_size, crossbar_size, conv_dt)
+
 if __name__ == "__main__":
     main()
