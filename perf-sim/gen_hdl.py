c0e853dbd892cabc2752d8541bfc1b830574c1b6
liou.hu@protonmail.com <liou.hu@protonmail.com>
2024-06-02 19:50:36 +0200

    rtl: gen_hdl conv and pool done

--- perf-sim/gen_hdl.py
+++ perf-sim/gen_hdl.py
@@ -13,6 +13,7 @@ def gen_hdl(param_dict_tuple, datatype_size, crossbar_size, rd_bus_width, obuf_b
         f'\tparameter DATA_SIZE = {datatype_size},\n'
         f'\tparameter BUS_WIDTH = {rd_bus_width},\n'
         f'\tparameter OBUF_BUS_WIDTH = {obuf_bus_width},\n'
+        f'\tparameter OBUF_DATA_SIZE = (DATA_SIZE == 1) ? $clog2(XBAR_SIZE) : 2*DATA_SIZE+$clog2(XBAR_SIZE),\n'
         f'\tparameter NUM_CHANNELS = $rtoi($floor(OBUF_BUS_WIDTH / OBUF_DATA_SIZE)),\n'
         f'\tparameter FIFO_LENGTH = $rtoi($ceil($floor(XBAR_SIZE / DATA_SIZE) / NUM_CHANNELS)),\n'
         f'\tparameter ELEMENTS_PER_TILE = $rtoi($floor(XBAR_SIZE / DATA_SIZE)),\n'
@@ -50,18 +51,18 @@ def gen_hdl(param_dict_tuple, datatype_size, crossbar_size, rd_bus_width, obuf_b
                             f'\tinput [OBUF_DATA_SIZE-1:0] L{n}_i_cim_data [L{n}_H_CIM_TILES-1:0][L{n}_NUM_CHANNELS-1:0][L{n}_V_CIM_TILES-1:0],\n'
                             f'\toutput [$clog2(NUM_ADDR_OBUF)-1:0] L{n}_o_cim_obuf_addr,\n'
                         )
+                        signals += (
+                            f'wire L{n}_next_ready;\n'
+                            f'wire [DATA_SIZE-1:0] L{n}_next_data [L{n}_H_CIM_TILES-1:0][L{n}_NUM_CHANNELS-1:0];\n'
+                            f'wire L{n}_next_we;\n'
+                            f'wire L{n}_next_start;\n'           
+                        )
                         if prev_layer[0] is None:
                             ports += (
                                 f'\tinput L{n}_i_ibuf_we,\n'
                                 f'\tinput [DATA_SIZE-1:0] L{n}_i_ibuf_wr_data [L{n}_H_CIM_TILES_IN-1:0][L{n}_NUM_CHANNELS-1:0],\n'
                                 f'\tinput L{n}_i_start,\n'
                                 f'\toutput L{n}_o_ready,\n'
-                            )
-                            signals += (
-                                f'wire L{n}_next_ready;\n'
-                                f'wire [DATA_SIZE-1:0] L{n}_next_data [L{n}_H_CIM_TILES-1:0][L{n}_NUM_CHANNELS-1:0];\n'
-                                f'wire L{n}_next_we;\n'
-                                f'wire L{n}_next_start;\n'           
                             )
                             modules += (
                                 f'fc_layer #(\n'
@@ -90,12 +91,6 @@ def gen_hdl(param_dict_tuple, datatype_size, crossbar_size, rd_bus_width, obuf_b
                                 f'\t.o_next_start(L{n}_next_start)\n);\n'
                             )
                         elif prev_layer[0] == "fc":
-                            signals += (
-                            f'wire L{n}_next_ready;\n'
-                            f'wire [DATA_SIZE-1:0] L{n}_next_data [L{n}_H_CIM_TILES-1:0][L{n}_NUM_CHANNELS-1:0];\n'
-                            f'wire L{n}_next_we;\n'
-                            f'wire L{n}_next_start;\n'           
-                            )
                             modules += (
                                 f'fc_layer #(\n'
                                 '\t.DATA_SIZE(DATA_SIZE),\n'
@@ -158,12 +153,11 @@ def gen_hdl(param_dict_tuple, datatype_size, crossbar_size, rd_bus_width, obuf_b
                             f') L{n}_fc_layer (\n'
                             '\t.clk(clk),\n'
                             '\t.rst(rst),\n'
-                            f'\t.i_ibuf_we(L{n}_ibuf_we),\n'
-                            f'\t.i_ibuf_data(L{n}_ibuf_wr_data),\n'
-                            f'\t.i_start(L{n}_start),\n'
-                            f'\t.o_ready(L{n}_ready),\n'
+                            f'\t.i_ibuf_we(L{n-1}_next_we),\n'
+                            f'\t.i_ibuf_data(L{n-1}_next_data),\n'
+                            f'\t.i_start(L{n-1}_next_start),\n'
+                            f'\t.o_ready(L{n-1}_next_ready),\n'
                             f'\t.i_cim_ready(L{n}_i_cim_ready),\n'
-                            f'\t.o_cim_wr_addr(L{n}_o_cim_wr_addr),\n'
                             f'\t.o_cim_data(L{n}_o_cim_data),\n'
                             f'\t.o_cim_we(L{n}_o_cim_we),\n'
                             f'\t.o_cim_rd_addr(L{n}_o_cim_rd_addr),\n'
@@ -177,19 +171,133 @@ def gen_hdl(param_dict_tuple, datatype_size, crossbar_size, rd_bus_width, obuf_b
                     case _:
                         raise Exception(f"Bad layers {prev_layer[0]} - {current_layer[0]}")
             case "conv":
+                parameters += (
+                    f'\tparameter L{n}_IMG_DIM = {current_layer[1]},\n'
+                    f'\tparameter L{n}_KERNEL_DIM = {current_layer[2]},\n'
+                    f'\tparameter L{n}_INPUT_CHANNELS = {current_layer[3]},\n'
+                    f'\tparameter L{n}_OUTPUT_CHANNELS = {current_layer[4]},\n'
+                    f'\tparameter L{n}_V_CIM_TILES = (L{n}_INPUT_CHANNELS*L{n}_KERNEL_DIM**2 + XBAR_SIZE-1) / XBAR_SIZE,\n'
+                    f'\tparameter L{n}_H_CIM_TILES = (L{n}_OUTPUT_CHANNELS * DATA_SIZE + XBAR_SIZE - 1) / XBAR_SIZE,\n'
+                    f'\tparameter L{n}_NUM_ADDR = $rtoi($ceil(L{n}_INPUT_CHANNELS*L{n}_KERNEL_DIM**2 / (BUS_WIDTH * L{n}_V_CIM_TILES))),\n'
+                    f'\tparameter L{n}_ADDR_WIDTH = (L{n}_NUM_ADDR <= 1) ? 1 : $clog2(L{n}_NUM_ADDR),\n'
+                    f'\tparameter L{n}_H_CIM_TILES = $rtoi($ceil(L{n}_OUTPUT_NEURONS * DATA_SIZE / XBAR_SIZE)),\n'
+                )
+                ports += (
+                    f'\toutput [BUS_WIDTH*L{n}_V_CIM_TILES-1:0] L{n}_o_cim_rd_data [DATA_SIZE-1:0]\n'
+                    f'\tinput L{n}_i_cim_ready,\n'
+                    f'\toutput L{n}_o_cim_we,\n'
+                    f'\toutput L{n}_o_cim_start,\n'
+                    f'\toutput [L{n}_ADDR_WIDTH-1:0] L{n}_o_cim_rd_addr,\n'
+                    f'\tinput [OBUF_DATA_SIZE-1:0] L{n}_i_cim_obuf_data [L{n}_H_CIM_TILES-1:0][NUM_CHANNELS-1:0][L{n}_V_CIM_TILES-1:0],\n'
+                    f'\toutput reg [$clog2(NUM_ADDR_OBUF)-1:0] L{n}_o_cim_obuf_addr,\n'
+                )
                 match prev_layer[0]:
-                    case "conv":
-                        pass
-                    case "pool":
-                        pass
-                    case None:
-                        pass
+                    case "conv" | "pool" | None:
+                        signals += (
+                            f'wire L{n}_next_ready;\n'
+                            f'wire [DATA_SIZE-1:0] L{n}_next_data [L{n}_H_CIM_TILES-1:0][L{n}_NUM_CHANNELS-1:0];\n'
+                            f'wire L{n}_next_we;\n'
+                            f'wire L{n}_next_start;\n'           
+                        )
+                        if prev_layer[0] is None:
+                            ports += (
+                                f'\tinput [INPUT_CHANNELS-1:0] L{n}_i_ibuf_we,\n'
+                                f'\tinput [DATA_SIZE-1:0] L{n}_i_ibuf_wr_data [L{n}_INPUT_CHANNELS-1:0],\n'
+                                f'\toutput L{n}_o_ready,\n'
+                                f'\tinput L{n}_i_start,\n'
+                            )
+                            modules += (
+                                'conv_layer #(\n'
+                                '\t.DATA_SIZE(DATA_SIZE),\n'
+                                f'\t.IMG_DIM(L{n}_IMG_DIM),\n'
+                                f'\t.KERNEL_DIM(L{n}_KERNEL_DIM),\n'
+                                f'\t.INPUT_CHANNELS(L{n}_INPUT_CHANNELS),\n'
+                                '\t.XBAR_SIZE(XBAR_SIZE),\n'
+                                f'\t.BUS_WIDTH(BUS_WIDTH)\n'
+                                f'\t.OUTPUT_CHANNELS(L{n}_OUTPUT_CHANNELS),\n'
+                                '\t.OBUF_BUS_WIDTH(OBUF_BUS_WIDTH)\n'
+                                f') L{n}_conv_layer (\n'
+                                '\t.clk(clk),\n'
+                                '\t.rst(rst),\n'
+                                f'\t.i_ibuf_we(L{n}_i_ibuf_we),\n'
+                                f'\t.i_ibuf_wr_data(L{n}_inbuf_wr_data),\n'
+                                f'\t.i_start(L{n-1}_i_start),\n'
+                                f'\t.o_ready(L{n-1}_o_ready),\n'
+                                f'\t.i_cim_ready(L{n}_i_cim_ready),\n'
+                                f'\t.o_cim_data(L{n}_o_cim_data),\n'
+                                f'\t.o_cim_we(L{n}_o_cim_we),\n'
+                                f'\t.o_cim_rd_addr(L{n}_o_cim_rd_addr),\n'
+                                f'\t.i_cim_data(L{n}_i_cim_data),\n'
+                                f'\t.o_cim_obuf_addr(L{n}_o_cim_obuf_addr),\n'
+                                f'\t.i_next_ready(L{n}_next_ready),\n'
+                                f'\t.o_next_data(L{n}_next_data),\n'
+                                f'\t.o_next_we(L{n}_next_we),\n'
+                                f'\t.o_next_start(L{n}_next_start)\n);\n'
+                            )
+                        else:
+                            modules += (
+                                'conv_layer #(\n'
+                                '\t.DATA_SIZE(DATA_SIZE),\n'
+                                f'\t.IMG_DIM(L{n}_IMG_DIM),\n'
+                                f'\t.KERNEL_DIM(L{n}_KERNEL_DIM),\n'
+                                f'\t.INPUT_CHANNELS(L{n}_INPUT_CHANNELS),\n'
+                                '\t.XBAR_SIZE(XBAR_SIZE),\n'
+                                f'\t.BUS_WIDTH(BUS_WIDTH)\n'
+                                f'\t.OUTPUT_CHANNELS(L{n}_OUTPUT_CHANNELS),\n'
+                                '\t.OBUF_BUS_WIDTH(OBUF_BUS_WIDTH)\n'
+                                f') L{n}_conv_layer (\n'
+                                '\t.clk(clk),\n'
+                                '\t.rst(rst),\n'
+                                f'\t.i_ibuf_we(L{n-1}_next_we),\n'
+                                f'\t.i_ibuf_wr_data(L{n-1}_next_data),\n'
+                                f'\t.i_start(L{n-1}_next_start),\n'
+                                f'\t.o_ready(L{n-1}_next_ready),\n'
+                                f'\t.i_cim_ready(L{n}_i_cim_ready),\n'
+                                f'\t.o_cim_data(L{n}_o_cim_data),\n'
+                                f'\t.o_cim_we(L{n}_o_cim_we),\n'
+                                f'\t.o_cim_rd_addr(L{n}_o_cim_rd_addr),\n'
+                                f'\t.i_cim_data(L{n}_i_cim_data),\n'
+                                f'\t.o_cim_obuf_addr(L{n}_o_cim_obuf_addr),\n'
+                                f'\t.i_next_ready(L{n}_next_ready),\n'
+                                f'\t.o_next_data(L{n}_next_data),\n'
+                                f'\t.o_next_we(L{n}_next_we),\n'
+                                f'\t.o_next_start(L{n}_next_start)\n);\n'
+                            )
                     case _:
                         raise Exception(f"{conf_name} Bad layer {n} {prev_layer[0]} - {current_layer[0]}")
             case "pool":
                 match prev_layer[0]:
                     case "conv":
-                        pass
+                        parameters += (
+                            f'\tparameter L{n}_IMG_DIM = {current_layer[1]},\n'
+                            f'\tparameter L{n}_INPUT_CHANNELS = {current_layer[3]},\n'
+                            f'\tparameter L{n}_KERNEL_DIM = {current_layer[2]},\n'
+                            f'\tparameter L{n}_OUTPUT_CHANNELS = L{n}_INPUT_CHANNELS,\n'
+                        )
+                        signals += (
+                            f'wire L{n}_next_ready;\n'
+                            f'wire [DATA_SIZE-1:0] L{n}_next_data [L{n}_OUTPUT_CHANNELS-1:0];\n'
+                            f'wire L{n}_next_we;\n'
+                            f'wire L{n}_next_start;\n'           
+                        )
+                        modules += (
+                            'pool_layer #(\n'
+                            '\t.DATA_SIZE(DATA_SIZE),\n'
+                            f'\t.IMG_DIM(L{n}_IMG_DIM),\n'
+                            f'\t.KERNEL_DIM(L{n}_KERNEL_DIM),\n'
+                            f'\t.INPUT_CHANNELS(L{n}_INPUT_CHANNELS)\n'
+                            f') L{n}_pool_layer (\n'
+                            '\t.clk(clk),\n'
+                            '\t.rst(rst),\n'
+                            f'\t.i_ibuf_we(L{n-1}_next_we),\n'
+                            f'\t.i_ibuf_wr_data(L{n-1}_next_data),\n'
+                            f'\t.i_start(L{n-1}_next_start),\n'
+                            f'\t.o_ready(L{n-1}_next_ready),\n'
+                            f'\t.i_next_ready(L{n}_next_ready),\n'
+                            f'\t.o_next_data(L{n}_next_data),\n'
+                            f'\t.o_next_we(L{n}_next_we),\n'
+                            f'\t.o_next_start(L{n}_next_start)\n);\n'
+                        )
                     case _:
                         raise Exception(f"{conf_name} Bad layer {n} {prev_layer[0]} - {current_layer[0]}")
             case None:
