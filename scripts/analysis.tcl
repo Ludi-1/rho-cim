ef97e2c399d826594ef7535473cb6c6e4f05e87b
Ludi-1 <liou.hu@protonmail.com>
2024-06-03 02:12:36 +0200

    rtl: update tcl script

--- scripts/analysis.tcl
+++ scripts/analysis.tcl
@@ -1,5 +1,8 @@
 open_project /home/li/Documents/Projects/rho-CIM2/rho-CIM2.xpr
-update_compile_order -fileset sources_1
+set_property board_part digilentinc.com:arty-a7-35:part0:1.1 [current_project]
+add_files -norecurse {/home/li/Documents/GitHub/rho-cim/hdl/flatten_fc_layer.sv /home/li/Documents/GitHub/rho-cim/hdl/conv_ibuf.sv /home/li/Documents/GitHub/rho-cim/hdl/pool_layer.sv /home/li/Documents/GitHub/rho-cim/hdl/fc_ctrl.sv /home/li/Documents/GitHub/rho-cim/hdl/flatten_fc_ibuf.sv /home/li/Documents/GitHub/rho-cim/hdl/flatten_fc_ctrl.sv /home/li/Documents/GitHub/rho-cim/hdl/conv_layer.sv /home/li/Documents/GitHub/rho-cim/hdl/fc_func.sv /home/li/Documents/GitHub/rho-cim/hdl/conv_ctrl.sv /home/li/Documents/GitHub/rho-cim/hdl/fc_layer.sv /home/li/Documents/GitHub/rho-cim/hdl/conv_func.sv /home/li/Documents/GitHub/rho-cim/hdl/fc_ibuf.sv}
+add_files -norecurse /home/li/Documents/GitHub/rho-cim/perf-sim/gen_hdl/vgg16_d8_c128.sv
+set_property top vgg16_d8_c128_top [current_fileset]
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 reset_run synth_1
 launch_runs synth_1 -jobs 16
