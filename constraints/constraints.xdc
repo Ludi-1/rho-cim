be06182d4590a8422c8b64350d2b47dc4b3a2b21
liou.hu@protonmail.com <liou.hu@protonmail.com>
2024-06-03 02:01:15 +0200

    rtl: report script

--- constraints/constraints.xdc
+++ constraints/constraints.xdc
@@ -1,1 +1,2 @@
 create_clock -add -name sys_clk_pin -period 4.00 -waveform {0 2} [get_ports { clk }];
+set_property HD.CLK_SRC BUFGCTRL_X0Y16 [get_ports clk]
--- scripts/analysis.tcl
+++ scripts/analysis.tcl
@@ -3,4 +3,8 @@ update_compile_order -fileset sources_1
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 reset_run synth_1
 launch_runs synth_1 -jobs 16
+wait_on_run synth_1
+open_run synth_1 -name synth_1
+report_utilization > /home/li/Documents/Projects/report/power.txt
+report_power > /home/li/Documents/Projects/report/power.txt
 exit
